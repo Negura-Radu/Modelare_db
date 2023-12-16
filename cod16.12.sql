c819c5b083140297c036b8c116641454206f4be8
Negura-Radu <149658915+Negura-Radu@users.noreply.github.com>
2023-12-16 09:56:20 +0200

    Add files via upload

---
+++ cod16.12.sql
@@ -0,0 +1,24 @@
+
+
+CREATE TABLE Adresa (
+    ID INT PRIMARY KEY,
+    strada VARCHAR(100),
+    oras VARCHAR(100),
+    tara VARCHAR(100)
+);
+
+CREATE TABLE Carte (
+    ID INT PRIMARY KEY,
+    titlu VARCHAR(100),
+    an_publicare INT,
+    ID_Autor INT,
+    FOREIGN KEY (ID_Autor) REFERENCES Autor(ID)
+);
+
+CREATE TABLE Review (
+    ID INT PRIMARY KEY,
+    rating INT,
+    comentariu TEXT,
+    ID_Carte INT,
+    FOREIGN KEY (ID_Carte) REFERENCES Carte(ID)
+);
